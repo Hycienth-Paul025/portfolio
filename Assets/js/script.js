2e4eaeedaf8ccc5c85151ad1ad35f6e01330cd2d
hycienth paul <hycienthp025@gmail.com>
2025-10-15 18:45:30 +0100

    project not done yet

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -31,11 +31,86 @@ document.addEventListener('DOMContentLoaded', () => {
     navOverlay.classList.add('hidden');
   });
 
+  //// Active Navliinks
+
+  function removeActive() {
+    navLinks.forEach((links) => {
+      links.classList.remove('active');
+    });
+  }
+
+  navLinks.forEach((links) => {
+    links.addEventListener('click', () => {
+      removeActive();
+      links.classList.add('active');
+    });
+  });
+
+  //// Show Active Section
+
+  window.addEventListener('scroll', () => {
+    const SectionEl = document.querySelectorAll('section[id]');
+
+    const scrollDown = window.scrollY;
+    SectionEl.forEach((sections) => {
+      const sectHeight = sections.offsetHeight;
+
+      const secTop = sections.offsetTop - 80;
+      const sectId = sections.getAttribute('id');
+
+      const sectLink = document.querySelector(
+        '#navbar ul li a[href*=' + sectId + ']'
+      );
+
+      if (scrollDown > secTop && scrollDown <= secTop + sectHeight) {
+        sectLink.classList.add('active');
+      } else {
+        sectLink.classList.remove('active');
+      }
+    });
+
+
+  });
+
+
+  
   /// Add shadow to header when scrolled down
 
   window.addEventListener('scroll', () => {
     window.scrollY >= 45
       ? headerEL.classList.add('shadow-xl')
       : headerEL.classList.remove('shadow-xl');
+  });
+
+  // Skills Swiper Function
+
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    speed: 600,
+    spaceBetween: 30,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true,
+    },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    grabCursor: true,
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+      },
+
+      760: {
+        slidesPerView: 2,
+      },
+
+      1024: {
+        slidesPerView: 3,
+      },
+    },
   });
 });
